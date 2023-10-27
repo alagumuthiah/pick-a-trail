ae1a7d299c587d4f53ad2766418fbdaf9807292d
alagumuthiah <alagudevi1996@gmail.com>
2023-10-27 13:16:30 -0700

    Added Oauth code and commented - TO BE CHECKED & INCLUDED

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+// const passport = require('passport');
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
@@ -53,9 +54,55 @@ router.post(
     }
 );
 
+//logout
 router.delete('/', (req, res) => {
     res.clearCookie('token');
     return res.json({ message: 'success' });
-})
+});
+
+// router.get('/login/sucess', (req, res, next) => {
+//     console.log('Login Sucess');
+//     res.send('Login Success');
+// });
+
+// router.get('/login/failed', (req, res, next) => {
+//     console.log('Login failed');
+//     res.send('Login failed');
+// });
+
+// router.get('/google',
+//     passport.authenticate('google', { scope: ['email', 'profile'] })
+// );
+
+// router.get('/google/callback',
+//     passport.authenticate('google', {
+//         successRedirect: CLIENT_URL,
+//         failureRedirect: '/auth/login/failed'
+//     })
+// );
+
+// router.get('/github',
+//     passport.authenticate('github', { scope: ['user:email'] })
+// );
+
+// router.get('/github/callback',
+//     passport.authenticate('github', {
+//         successRedirect: `${CLIENT_URL}/explore`,
+//         failureRedirect: '/auth/login/failed'
+//     })
+// );
+
+// //check how to specify scope to get relevant details from the user based on facebook profile
+// router.get('/facebook',
+//     passport.authenticate('facebook')
+// );
+
+// router.get('/facebook/callback',
+//     passport.authenticate('facebook', {
+//         successRedirect: `${CLIENT_URL}/explore`,
+//         failureRedirect: '/auth/login/failed'
+//     })
+// );
+
 
 module.exports = router;
