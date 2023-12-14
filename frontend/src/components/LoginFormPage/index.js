36c26b743e3c3a618c5b7372bf4c591c98f09eaa
alagumuthiah <alagudevi1996@gmail.com>
2023-12-13 16:19:17 -0800

    Frontend component for Login and Signup

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -1,29 +1,65 @@
-import { baseUrl } from '../../utils/baseUrl';
-import { Link } from 'react-router-dom';
-
+// import { baseUrl } from '../../utils/baseUrl';
+// import { Link } from 'react-router-dom';
+import { useState } from 'react';
 export default function Login() {
 
-    const googlePage = () => {
-        window.open(`${baseUrl}/auth/google`, "_self");
-    }
+    // const googlePage = () => {
+    //     window.open(`${baseUrl}/auth/google`, "_self");
+    // }
 
-    const githubPage = () => {
-        window.open(`${baseUrl}/auth/github`, "_self");
-    }
+    // const githubPage = () => {
+    //     window.open(`${baseUrl}/auth/github`, "_self");
+    // }
 
-    const facebookPage = () => {
-        window.open(`${baseUrl}/auth/facebook`, "_self");
+    // const facebookPage = () => {
+    //     window.open(`${baseUrl}/auth/facebook`, "_self");
+    // }
+    const [credential, setCredential] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = (event) => {
+        console.log('handle Login');
+        event.preventDefault();
+        const payload = {
+            credential,
+            password
+        }
+        console.log(payload);
     }
     return (
-        <div className="login__form">
-            <h2>Login</h2>
-            <br />
-            <br />
-            <br />
-            <button onClick={googlePage}>Google</button>
-            <button onClick={githubPage}>Github</button>
-            <button onClick={facebookPage}>Facebook</button>
+        <div className="container">
+            <h2>Login and Start Exploring</h2>
+            <form className="form" onSubmit={handleLogin}>
+                <div>
+                    <label htmlFor="credential">UserName / Email</label>
+                    <input
+                        name="credential"
+                        type="text"
+                        value={credential}
+                        onChange={(event) => setCredential(event.target.value)} />
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input
+                        name="password"
+                        type="password"
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)} />
+                </div>
+                <div>
+                    <button type="submit">Login</button>
+                </div>
+            </form>
+            <hr></hr>
+            <button>Google</button>
+            <button>Github</button>
+            <button>Facebook</button>
+            <hr>
+            </hr>
+            <h4>Don't have an account? <a href="/signup">Sign Up</a> </h4>
         </div>
+
+
 
     )
 }
--- frontend/src/components/SignUpFormPage/index.js
+++ frontend/src/components/SignUpFormPage/index.js
@@ -1,8 +1,75 @@
 
+import { useState } from "react";
+
+//firstName, lastName, userName, email, password
 export default function SignUp() {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [userName, setUserName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSignup = (event) => {
+        event.preventDefault();
+        const payload = {
+            firstName,
+            lastName,
+            userName,
+            email,
+            password
+        }
+        console.log(payload);
+    }
     return (
-        <h1>
-            SignUp
-        </h1>
+
+        <div className="container">
+            <h2>Sign up today to start planning your next adventure</h2>
+            <form className="form" onSubmit={handleSignup}>
+                <div>
+                    <label htmlFor="firstname">FirstName</label>
+                    <input
+                        name="firstname"
+                        type="text"
+                        value={firstName}
+                        onChange={(event) => setFirstName(event.target.value)} />
+                </div>
+                <div>
+                    <label htmlFor="lastname">LastName</label>
+                    <input
+                        name="lastname"
+                        type="text"
+                        value={lastName}
+                        onChange={(event) => setLastName(event.target.value)} />
+                </div>
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input
+                        name="email"
+                        type="email"
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)} />
+                </div>
+                <div>
+                    <label htmlFor="username">UserName</label>
+                    <input
+                        name="username"
+                        type="text"
+                        value={userName}
+                        onChange={(event) => setUserName(event.target.value)} />
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input
+                        name="password"
+                        type="password"
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)} />
+                </div>
+                <div>
+                    <button type="submit">Sign Up</button>
+                </div>
+            </form>
+        </div>
+
     )
 }
